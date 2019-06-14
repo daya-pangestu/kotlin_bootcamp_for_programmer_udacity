a194488337fcb64cec3a97c5aab497d0dabf0a5b
dayfrk <day.pangestu@gmail.com>
2019-06-14 09:32:09 +0700

    practice time, improve whatShouldIDoToday()

--- src/suggestActivity.kt
+++ src/suggestActivity.kt
@@ -3,8 +3,16 @@ fun main() {
 }
 
 fun whatSOuldIDoToday(mood: String, weather: String = "sunny", temperature: Int = 24) :String{
-    return when  {
-        mood == "happy" && weather == "sunny" -> "go for a walk"
-        else -> "stay home amd read"
+    return when {
+        isVeryHot(temperature) -> "go swimming"
+        isSadRainyCold(mood, weather, temperature) -> "stay in bed"
+        isHappySunny(mood,weather) -> "go for a walk"
+        else -> "stay home and read"
     }
+
+
 }
+fun isVeryHot(temperature: Int) = temperature > 35
+fun isSadRainyCold(mood: String, wheather: String, temperature: Int) =
+    mood == "sad" && "wheater" == "rainy" && temperature == 0
+fun isHappySunny(mood:String,wheather: String) = mood == "happy" && wheather =="sunny"
